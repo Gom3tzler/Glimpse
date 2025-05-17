702f90be669db7613e7ba0aecf10416b95535c89
Jereme <michael@jeremehancock.com>
2025-05-16 19:17:11 -0500

    updated play icon

--- web/sw.js
+++ web/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Glimpse Media Viewer
 
-const CACHE_NAME = "glimpse-media-viewer-v6";
-const DYNAMIC_CACHE = "glimpse-media-dynamic-v6";
+const CACHE_NAME = "glimpse-media-viewer-v6.2";
+const DYNAMIC_CACHE = "glimpse-media-dynamic-v6.2";
 
 // Assets to cache on install
 const STATIC_ASSETS = ["/", "/index.html", "/manifest.json", "/test.html"];
